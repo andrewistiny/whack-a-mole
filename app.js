21a21861908ad14ed8b3b629c4aabfa4d9850629
andrewistiny <aleachman3619@myhta.org>
2018-03-29 14:20:48 -1000

    Stretch Goals Whack a trump

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 window.onload = function() {
     var dirt = document.getElementsByClassName("dirt");
     var sound = document.getElementById("whack");
+    var lose = document.getElementById("lose");
     var grid = document.getElementById("dirt-box");
-
+    var score = 0;
     for (let i = 0; i < dirt.length ; i++ ){
         dirt[i].addEventListener("click", function(){
             if(dirt[i].innerHTML){
+                score = score + 1;
                 dirt[i].innerHTML = "";
                 sound.play();
+                document.getElementById("score").innerHTML = "Congratz broski, you have won a small loan of (" + score + ") million dollars!"
+                if (score  > 1000000){
+                    document.getElementById("score").innerHTML = "Congratz broski, you have no life"
+                }
             } else {
-                console.log("Nothing There");
+                lose.play();
+                alert("Your Dumb, trump isn't there. You Lose :D");
+                location.reload();
             }
      })
     }
